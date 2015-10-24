3e155dc0d54b167691b7c833988506a268c9bd67
Philipp Grigoryev <philipp.grigoryev@icsynergy.com>
2015-10-23 23:59:15 -0500

    added monthly stat

--- src/main/groovy/com/grigorio/smsserver/controller/StatusController.groovy
+++ src/main/groovy/com/grigorio/smsserver/controller/StatusController.groovy
@@ -6,8 +6,11 @@ import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import java.time.LocalDateTime
+import java.time.Month
+import java.time.format.TextStyle
 
 @Slf4j
 @Controller
@@ -31,5 +34,28 @@ class StatusController {
 
         log.trace '<< showStatus'
         return 'current'
+    }
+
+    /**
+     * Produces monthly statistics
+     * @param model
+     * @param month
+     * @return
+     */
+    @RequestMapping(value = '/stat/month/{month}', produces = 'text/html; charset=utf-8')
+    String showMonthlyStat(Model model, @PathVariable int month) {
+        log.trace '>> showMonthlyStat'
+        log.debug "showMonthlyStat: $month"
+
+        log.trace 'querying DB'
+        List<Object[]> lst = smsRepo.getStatByMonth(month)
+
+        log.debug "Data from DB: $lst"
+
+        model.addAttribute('month', Month.of(month).getDisplayName(TextStyle.FULL_STANDALONE, new Locale('ru', 'RU')))
+        model.addAttribute('stat', lst)
+
+        log.trace '<< showMonthlyStat'
+        return 'monthlyStat'
     }
 }
--- src/main/groovy/com/grigorio/smsserver/repository/SmsRepository.groovy
+++ src/main/groovy/com/grigorio/smsserver/repository/SmsRepository.groovy
@@ -2,13 +2,21 @@ package com.grigorio.smsserver.repository
 
 import com.grigorio.smsserver.domain.Sms
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
 import org.springframework.data.repository.CrudRepository
 
 import java.time.LocalDateTime
 
+@Repository
 interface SmsRepository extends CrudRepository<Sms, Long>{
-    List<Sms> findByAddress(String addr)
     List<Sms> findByAddressAndTsAfterOrderByTsDesc(String addr, LocalDateTime dt)
 
     List<Sms> findByTsAfterAndStatusNotOrderByTsDesc(LocalDateTime dt, char status)
+
+    //statistics
+    @Query(value = 'select status, count(*), sum(parts) from sms where month(ts) = :m group by status',
+            nativeQuery = true)
+    List<Object[]> getStatByMonth(@Param('m') int month)
 }
